968a06c58df005021c4691b0be8662e59884d8e4
NyxIsBad <tbnrimmapotato@gmail.com>
2024-12-04 11:12:36 -0500

    SLIC with RadiusGraph

--- slic.py
+++ slic.py
@@ -1,4 +1,4 @@
-import torch
+import PIL.Image
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
@@ -11,25 +11,42 @@ from torch_geometric.data import Data
 
 from utils import *
 
+# ------------------- SLIC ------------------- #
+IMAGE_DIR = './'
+SIZE = (224, 224)
+N_SEGMENTS = 500
+COMPACTNESS = 1
+
 print('Loading data...')
 
 # Load images from the directory
-images = load_dir('./')  # Replace with the path to your image directory
-# resize to 224x224
-images = [cv2.resize(image, (224, 224)) for image in images]
+images = load_dir(IMAGE_DIR)  # Replace with the path to your image directory
+images = [cv2.resize(image, SIZE) for image in images]
+
 sample_image = images[0]
 
-# show the image in PIL
-PIL.Image.fromarray(sample_image).show()
+slicified = slicify(sample_image, n_segments=N_SEGMENTS, compactness=COMPACTNESS, radius=SIZE[0]//8, max_neighbors=32)
 
-graph = slicify(sample_image, n_segments=50, compactness=10)
+print(slicified)
 
-# print graph
-print(graph)
+slicified_image = superpixels_to_2d_image(rec=slicified, scale=10, edge_width=5, size_tuple=SIZE, bg=sample_image)
+slicified_graph = superpixels_to_2d_image(rec=slicified, scale=10, edge_width=5, size_tuple=SIZE, bg=None)
 
-# plot graph
+# plot the 3 images
+plt.figure(figsize=(15, 15))
+plt.subplot(131)
 plt.imshow(sample_image)
-# Show graph boundaries
-
+plt.title('Original Image')
 plt.axis('off')
+
+plt.subplot(132)
+plt.imshow(slicified_image)
+plt.title('Superpixels')
+plt.axis('off')
+
+plt.subplot(133)
+plt.imshow(slicified_graph)
+plt.title('Graph Connectivity')
+plt.axis('off')
+
 plt.show()
